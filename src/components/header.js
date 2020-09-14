54448fa93ae3c126712eb5f30f9b283496d589c0
Almin Islamović <almin.islamovic@gmail.com>
2020-09-14 09:19:10 +0000

    Added GA

--- src/components/header.js
+++ src/components/header.js
@@ -73,6 +73,14 @@ const Header = props => {
             />
           </span>
         </div>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-56814604-5"></script>
+        <script>
+          window.dataLayer = window.dataLayer || []
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date())
+
+          gtag('config', 'UA-56814604-5')
+        </script>
       </header>
     </>
   )
