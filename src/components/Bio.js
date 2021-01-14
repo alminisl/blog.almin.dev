e8d0163157e9b1d1fa2aa3d347e908fdb307c307
Almin Islamovic <almin.islamovic@gmail.com>
2021-01-14 20:03:06 +0100

    Fix

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './Bio.module.css'
-import { getGravatarURL } from '../utils/getGravatarURL'
+// import { getGravatarURL } from '../utils/getGravatarURL'
 
 function Bio(props) {
-  let photoURL = getGravatarURL({
-    email: "test1@example.com",
-    size: 56,
-  })
+  // let photoURL = getGravatarURL({
+  //   email: "test1@example.com",
+  //   size: 56,
+  // })
 
   return (
     <div className={`
