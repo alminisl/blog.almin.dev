5fc2cc4563259030008f1393699886f20d4ac176
Almin Islamovic <almin.islamovic@gmail.com>
2021-01-21 21:43:29 +0100

    Update

--- src/routes/posts/2021-01-20-budgetstreamdeck/post.js
+++ src/routes/posts/2021-01-20-budgetstreamdeck/post.js
@@ -1,5 +1,5 @@
 export default {
-    title: `Creating your own, budget - DIY streamdeck / macro keyboard with JS`,
+    title: `Creating your own, budget, streamdeck / macro keyboard with JS`,
     tags: ['DIY', 'project', 'iot', 'javascript'],
     spoiler: "Beginner friendly Arduino project",
     getContent: () => import('./document.mdx'),
