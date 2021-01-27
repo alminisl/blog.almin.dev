73a4bbcf9850b05ca61cf6ed65b051b398911d54
Almin Islamovic <almin.islamovic@gmail.com>
2021-01-27 10:46:20 +0100

    cookie consent

--- src/components/BlogIndexPage.js
+++ src/components/BlogIndexPage.js
@@ -5,6 +5,7 @@ import ArticleSummary from './ArticleSummary'
 import Bio from './Bio'
 import Pagination from './Pagination'
 import styles from './BlogIndexPage.module.css'
+import CookieConsent from "react-cookie-consent";
 import './card.css'
 
 function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
@@ -30,6 +31,23 @@ function BlogIndexPage({ blogRoot, pageCount, pageNumber, postRoutes }) {
           pageNumber={pageNumber}
         />
       )}
+      <CookieConsent
+                location="bottom"
+                buttonText="Accept and Close"
+                cookieName="consentCookie"
+                style={{ background: "#2B373B" }}
+                buttonStyle={{ color: "#4e503b", fontSize: "15px" }}
+                expires={150}
+                onAccept={() => {
+                  window.location.reload(false);
+                }}
+                enableDeclineButton
+                onDecline={() => {
+                  console.log("Cookies disabled!")
+                }}
+        >
+          This blog uses anonymus analytics cookie to keep track on most visited blogposts to give me some insights. If you don't want to participate feel free to decline! :)
+        </CookieConsent>
       <footer className={styles.footer}>
         <div>
           <a href="/rss.xml" target="_blank" style={{ float: 'right' }}>
