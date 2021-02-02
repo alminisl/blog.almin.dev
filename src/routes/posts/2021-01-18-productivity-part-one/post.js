eaa8ddf913ff77d584a9f8aedd33af40867d2b18
Almin Islamovic <almin.islamovic@gmail.com>
2021-02-02 16:56:06 +0100

    add new post

--- src/routes/posts/2021-01-18-productivity-part-one/post.js
+++ src/routes/posts/2021-01-18-productivity-part-one/post.js
@@ -1,6 +1,6 @@
 export default {
     title: 'Developer productivity (part 1)',
     tags: ['personal', 'project', 'productivity'],
-    spoiler: "Productivity overview..",
+    spoiler: "Productivity overview",
     getContent: () => import('./document.mdx'),
   }
