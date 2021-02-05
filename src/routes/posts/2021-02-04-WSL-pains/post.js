0e9d8071069eac13c99f1f23e4d8692be69f1bbc
Almin Islamović <almin.islamovic@gmail.com>
2021-02-05 08:51:07 +0000

    Update post.js

--- src/routes/posts/2021-02-04-WSL-pains/post.js
+++ src/routes/posts/2021-02-04-WSL-pains/post.js
@@ -1,5 +1,5 @@
 export default {
-    title: 'Some post mortem on WSL Networking which I had to experience yesterday. ',
+    title: 'The pain of Networking with WSL2 and how to Fix it',
     tags: ['project', 'WSL'],
     spoiler: "Tools and Worlflows",
     getContent: () => import('./document.mdx'),
